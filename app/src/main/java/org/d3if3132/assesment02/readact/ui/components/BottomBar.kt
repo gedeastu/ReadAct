7785387b28667d9ccfc6ac1f0811327dcdeaf0a4
Gede Astu Nugraha <gede.astu@gmail.com>
2024-05-04 11:47:15 +0700

    make layout screen

---
+++ app/src/main/java/org/d3if3132/assesment02/readact/ui/components/BottomBar.kt
@@ -0,0 +1,8 @@
+package org.d3if3132.assesment02.readact.ui.components
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun BottomBar() {
+    
+}
--- app/src/main/java/org/d3if3132/assesment02/readact/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3132/assesment02/readact/ui/screen/MainScreen.kt
@@ -1,8 +1,17 @@
 package org.d3if3132.assesment02.readact.ui.screen
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import org.d3if3132.assesment02.readact.ui.components.BottomBar
 
 @Composable
-fun MainScreen() {
-    
+fun MainScreen(navHostController: NavHostController,content:@Composable (Modifier)->Unit) {
+    Scaffold(bottomBar = {
+        BottomBar()
+    }){paddingValues ->
+        content(Modifier.padding(paddingValues = paddingValues))
+    }
 }
